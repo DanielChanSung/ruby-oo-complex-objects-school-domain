b6773d10c225b61bf59c6a8fd93f6f5aa232a6d0
Daniel Sung <DanielChanSung@gmail.com>
2021-02-03 00:03:23 -0500

    commit tree 8057f65427e7407d24654dad0d70ae9ff1083145

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+
+    attr_accessor :roster
+    def initialize(sam)
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def grade(number)
+        @roster[number]
+    end
+
+    def sort
+        hash = {}
+        @roster.keys.sort.each do |number|
+        hash[number] = @roster[number].sort
+        end
+    hash
+    end
+
+end
+
+
